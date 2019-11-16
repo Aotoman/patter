e2151e5efdbb5e4777ea8782a9c9fecb2ca375aa
Jie <1209559402@qq.com>
2019-11-16 17:05:29 +0800

    the really first push

---
+++ src/test/java/com/uestc/auto/pattern/PatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uestc.auto.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
